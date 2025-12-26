e74b19186f01d2240d4071486a02d9aef33fc4c3
shujanislam <shujanm.islam231@gmail.com>
2025-12-27 02:28:21 +0530

    added projects count

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,6 +1,22 @@
 import "server-only";
 import { createSupabaseServer } from "./supabase/server";
 
+export const fetchProjectsCount = async() => {
+  try{
+    const supabase = await createSupabaseServer();
+
+    const { count, error } = await supabase.from("projects").select("*", { count: "exact", head: true });
+
+    if(error) throw new Error('Failed to fetch projects count');
+
+    return count;
+  }
+  catch(err: any){
+    console.log(err.message);
+    return 0;
+  }
+}
+
 export const fetchProjects = async() => {
   try{
     const supabase = await createSupabaseServer();
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,14 @@
-import { fetchProjects } from "../../../lib/queries";
+import { fetchProjects, fetchProjectsCount } from "../../../lib/queries";
 import CreateProjectCard from "../../components/projects";
 import ProjectGrid from '../../components/ui/ProjectGrid';
 
 export default async function ProjectsPage() {
   const projects = await fetchProjects();
+  const projects_count = await fetchProjectsCount();
 
   return (
     <div className="min-h-screen bg-white p-6 space-y-6">
-      <h2 className="text-xl font-semibold">Projects</h2>
+      <h2 className="text-xl font-semibold">Total Projects: {projects_count}</h2>
 
       <ProjectGrid projects={projects ?? []} />
 
